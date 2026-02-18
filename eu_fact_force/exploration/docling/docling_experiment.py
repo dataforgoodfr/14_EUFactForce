fe5d7f8535546fd860fd8fced147a2eb9bd0f032
Hugo De Oliveira <80337112+hugros-93@users.noreply.github.com>
2026-02-18 10:59:18 +0100

    add pypdf2 benchmark

--- eu_fact_force/exploration/docling/docling_experiment.py
+++ eu_fact_force/exploration/docling/docling_experiment.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from tqdm import tqdm
 from docling.document_converter import DocumentConverter
 from hierarchical.postprocessor import ResultPostprocessor
+from PyPDF2 import PdfReader
 
 
 def get_doc_list(doc_path):
@@ -14,7 +15,7 @@ def get_doc_list(doc_path):
     return list_files
 
 
-def run_mini_benchmark():
+def run_mini_benchmark_docling():
     # Get list of files
     doc_path = Path("docs/")
     list_files = get_doc_list(doc_path)
@@ -24,6 +25,7 @@ def run_mini_benchmark():
     converter = DocumentConverter()
 
     # Run experiment
+    print(f"### Starting - docling benchmark ###")
     experiment_results = {}
     for filename in tqdm(list_files):
         # Select file
@@ -44,22 +46,71 @@ def run_mini_benchmark():
             "time_per_page": total_time / len(result.pages),
             "total_chars": len(result.document.export_to_text()),
         }
-        with open("results/mini_benchmark_results.json", "w") as f:
+        with open("results/mini_benchmark_results_docling.json", "w") as f:
             json.dump(experiment_results, f)
 
         # Export files
         doc_markdwon = result.document.export_to_markdown()
-        with open(f"results/md/{doc_name}.md", "w", encoding="utf-8") as f:
+        with open(f"results/md/{doc_name}_docling.md", "w", encoding="utf-8") as f:
             f.write(doc_markdwon)
 
         doc_html = result.document.export_to_html()
-        with open(f"results/html/{doc_name}.html", "w", encoding="utf-8") as f:
+        with open(f"results/html/{doc_name}_docling.html", "w", encoding="utf-8") as f:
             f.write(doc_html)
 
         doc_json = result.document.export_to_dict()
-        with open(f"results/json/{doc_name}.json", "w", encoding="utf-8") as f:
+        with open(f"results/json/{doc_name}_docling.json", "w", encoding="utf-8") as f:
+            json.dump(doc_json, f, ensure_ascii=False)
+
+
+def run_mini_benchmark_pypdf2():
+    # Get list of files
+    doc_path = Path("docs/")
+    list_files = get_doc_list(doc_path)
+    print(f"> number of files: {len(list_files)}")
+
+    # Run experiment
+    print(f"### Starting - pypdf2 benchmark ###")
+    experiment_results = {}
+    for filename in tqdm(list_files):
+        # Select file
+        print(f"> selected file: {filename}")
+        file_path = Path(filename)
+        doc_name = file_path.stem
+
+        # Convert
+        start_time = time.time()
+        result = PdfReader(doc_path / file_path)
+        total_time = time.time() - start_time
+
+        # Get full text
+        full_text = []
+        for page in result.pages:
+            text = page.extract_text()
+            if text:
+                full_text.append(text)
+        full_text = "\n".join(full_text)
+
+        # Save results
+        experiment_results[filename] = {
+            "total_time": total_time,
+            "total_pages": len(result.pages),
+            "time_per_page": total_time / len(result.pages),
+            "total_chars": len(full_text),
+        }
+        with open("results/mini_benchmark_results_pypdf2.json", "w") as f:
+            json.dump(experiment_results, f)
+
+        # Export files
+        pages = []
+        for i, page in enumerate(result.pages, start=1):
+            pages.append({"page": i, "text": page.extract_text() or ""})
+
+        doc_json = {"num_pages": len(result.pages), "pages": pages}
+        with open(f"results/json/{doc_name}_pypdf2.json", "w", encoding="utf-8") as f:
             json.dump(doc_json, f, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    run_mini_benchmark()
+    run_mini_benchmark_docling()
+    run_mini_benchmark_pypdf2()
--- eu_fact_force/exploration/docling/readme.md
+++ eu_fact_force/exploration/docling/readme.md
@@ -1,20 +1,21 @@
 # EU Fact Force - Ingestion - Experiment - Docling 
 
-### Structure
+### Contenu
 - `notebook.ipynb`: notebook d'exploration de la structure du parsing produit par Docling, et execution d'un mini-benchmark.
-- `docling_experiment.py`: code du mibi-benchmark (temps d'execution, pages, quantité de texte, export des resultats json, markdown et html)
-- `docs/`: les PDFs de ce mini-benchmark
+- `docling_experiment.py`: code du mini-benchmark (`Docling` vs `PyPDF2` en terme de temps d'execution, pages, quantité de texte, export des resultats json, ainsi que markdown et html pour Docling)
+- `docs/`: les documents PDF utilisés pour ce mini-benchmark
 - `results`: Exports des résultats du benchmark, avec:
-    - `mini_benchmark_results.json`: les résultats 
+    - `mini_benchmark_results_docling.json`: les résultats Docling 
+    - `mini_benchmark_results_pypdf2.json`: les résultats PyPDF2
     - `json/`: exports JSON
     - `html/`: exports HTML
     - `md/`: exports markdwon
 
 ### Remarques
-- Testé avec tous les PDFs présents sur Kdrive en local, pour certains codument les temps d'executions sont très long (plusieurs dizaines de minutes pour un fichier...) 
-- De ce que je comprends, la methode d'OCR derrière peut être assez lourde. Ça peut être un problème si l'utilisateur doit attendre 10min avant de pouvoir éditer / visualiser le document.
+- Testé avec tous les PDFs présents sur Kdrive en local, pour certains documents les temps d'exécutions sont très long (plusieurs dizaines de minutes pour un fichier...) 
+- De ce que je comprends, la méthode d'OCR derrière peut être assez lourde. Ça peut être un problème si l'utilisateur doit attendre 10min avant de pouvoir éditer / visualiser le document.
 
 ### Next 
 - Ajouter Docling au benchmark complet
-- Investiguer une métrique d'évaluation de la qualité des sections / sous-sections identifiées
-- Convertir les résultats en une list de chunks comme attendu (metadata du document, page du chunk, contenu de l'extrait, metadata des sections correspondantes...)
+- Investiguer une métrique d'évaluation de la qualité des sections / sous-sections identifiées (ex: est-ce que ce text est bien détecté comme appartenant à cette section?). Le libraire de post-processing retourne des warnings qui pourraient être utilisés comme indicateur de qualité. 
+- Convertir les résultats en une liste de chunks comme attendu (metadata du document, page du chunk, contenu de l'extrait, metadata des sections correspondantes...)
