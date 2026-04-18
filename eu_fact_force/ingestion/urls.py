37c702f8b032ec473b209e09389e6f3218951ac6
Christophe Goudet <goudetdata@gmail.com>
2026-04-18 13:58:21 +0200

    remove comment

--- eu_fact_force/ingestion/urls.py
+++ eu_fact_force/ingestion/urls.py
@@ -6,7 +6,6 @@ app_name = "ingestion"
 urlpatterns = [
     path("ingest/", views.ingest, name="ingest"),
     path("search/<str:keyword>/", views.search, name="search"),
-    # API
     path("api/ingest/", views.api_ingest, name="api_ingest"),
     path("api/ingest/<int:pk>/pdf/", views.api_attach_pdf, name="api_attach_pdf"),
 ]
