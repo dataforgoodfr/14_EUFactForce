6e7bc39968f1937bf1802a4736454ca383f43aba
assitan-h <assitanniare325@gmail.com>
2026-04-26 22:07:26 +0200

    Add dash app structure

---
+++ pipeline_pgp/dash_app/layout.py
@@ -0,0 +1,59 @@
+from dash import html, dcc
+
+def create_layout():
+    return html.Div(
+        style={
+            "backgroundColor": "#FFFFFF",
+            "fontFamily": "Arial",
+            "padding": "20px"
+        },
+
+        children=[
+
+            html.Div(
+                children=[
+                    html.H1(
+                        "European Vaccine Dashboard",
+                        style={
+                            "color": "white",
+                            "textAlign": "center",
+                            "margin": "0"
+                        }
+                    )
+                ],
+                style={
+                    "backgroundColor": "#00669B",
+                    "padding": "15px",
+                    "borderRadius": "8px"
+                }
+            ),
+
+            html.Br(),
+
+            html.Div(
+                children=[
+                    html.H3("Filters"),
+                    html.P("Add dropdowns / filters here")
+                ],
+                style={
+                    "backgroundColor": "#E6F4F9",
+                    "padding": "15px",
+                    "borderRadius": "8px"
+                }
+            ),
+
+            html.Br(),
+
+            html.Div(
+                children=[
+                    html.H3("Visualizations"),
+                    html.P("Graphs will be added here")
+                ],
+                style={
+                    "backgroundColor": "#F5F5F5",
+                    "padding": "20px",
+                    "borderRadius": "8px"
+                }
+            )
+        ]
+    )
