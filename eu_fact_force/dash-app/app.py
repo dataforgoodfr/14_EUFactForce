45b8a2efd7e44ea68ad3c8533020f7222d657adc
Hugo De Oliveira <80337112+hugros-93@users.noreply.github.com>
2026-04-30 17:16:41 +0200

    fix issue #88 : split start and end date conditions, and check if "date" is available in nodes to filter

--- eu_fact_force/dash-app/app.py
+++ eu_fact_force/dash-app/app.py
@@ -318,20 +318,59 @@ def update_graph_and_list(
             }
 
         # Filter dates
-        if start_date or end_date:
+        if start_date:
             nodes = {
                 n: nodes[n]
                 for n in nodes
                 if nodes[n]["data"]["type"] not in ("chunk", "document")
                 or (
                     nodes[n]["data"]["type"] == "chunk"
-                    and nodes[n]["data"]["document_metadata"]["date"] >= start_date
-                    and nodes[n]["data"]["document_metadata"]["date"] <= end_date
+                    and (
+                        (
+                            "date" in nodes[n]["data"]["document_metadata"]
+                            and nodes[n]["data"]["document_metadata"]["date"]
+                            >= start_date
+                        )
+                        or "date" not in nodes[n]["data"]["document_metadata"]
+                    )
                 )
                 or (
                     nodes[n]["data"]["type"] == "document"
-                    and nodes[n]["data"]["metadata"]["date"] >= start_date
-                    and nodes[n]["data"]["metadata"]["date"] <= end_date
+                    and (
+                        (
+                            "date" in nodes[n]["data"]["metadata"]
+                            and nodes[n]["data"]["metadata"]["date"] >= start_date
+                        )
+                        or "date" not in nodes[n]["data"]["metadata"]
+                    )
+                )
+            }
+
+        if end_date:
+            nodes = {
+                n: nodes[n]
+                for n in nodes
+                if nodes[n]["data"]["type"] not in ("chunk", "document")
+                or (
+                    nodes[n]["data"]["type"] == "chunk"
+                    and (
+                        (
+                            "date" in nodes[n]["data"]["document_metadata"]
+                            and nodes[n]["data"]["document_metadata"]["date"]
+                            <= end_date
+                        )
+                        or "date" not in nodes[n]["data"]["document_metadata"]
+                    )
+                )
+                or (
+                    nodes[n]["data"]["type"] == "document"
+                    and (
+                        (
+                            "date" in nodes[n]["data"]["metadata"]
+                            and nodes[n]["data"]["metadata"]["date"] <= end_date
+                        )
+                        or "date" not in nodes[n]["data"]["metadata"]
+                    )
                 )
             }
 
