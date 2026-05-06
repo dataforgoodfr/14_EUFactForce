23750c1b51308a5a63f3a811e64d2816d5ea1ff1
Hugo De Oliveira <80337112+hugros-93@users.noreply.github.com>
2026-05-06 10:43:47 +0200

    make pgp page like other pages

--- eu_fact_force/dash-app/pages/pgp.py
+++ eu_fact_force/dash-app/pages/pgp.py
@@ -15,22 +15,26 @@ def make_layout():
             html.Div(
                 [
                     html.H1(
-                        "PGP Dashboard",
+                        "EU Fact Force - PGP Dashboard",
+                        className="mb-3 text-center",
                         style={
-                            "fontFamily": "system-ui, -apple-system, sans-serif",
-                            "textAlign": "center",
-                            "fontSize": "22px",
-                            "fontWeight": "bold",
-                        },
+                            "color": EUPHAColors.text_main,
+                            "fontWeight": "800",
+                            "marginBottom": "10px", 
+                            "fontSize": "36px"
+                        }
                     ),
                     html.P(
                         "This dashboard summarizes posting dynamics, linguistic distribution, platform sources, and key thematic patterns extracted from the dataset.",
+                        className="text-center mb-5",
                         style={
-                            "fontFamily": "system-ui, -apple-system, sans-serif",
-                            "textAlign": "center",
-                            "fontSize": "14px",
-                            "fontStyle": "italic",
-                        },
+                            "fontSize": "17px", 
+                            "color": EUPHAColors.text_main, 
+                            "marginTop": "20px", 
+                            "lineHeight": "1.6", 
+                            "maxWidth": "800px",
+                            "margin": "20px auto 0 auto"
+                        }
                     ),
                 ],
                 style={
@@ -97,4 +101,13 @@ def make_layout():
                 ],
             ),
         ],
+        style=
+        {
+            "borderRadius": "16px",
+            "padding": "60px",
+            "backgroundColor": EUPHAColors.white, 
+            "color": EUPHAColors.text_main,
+            "boxShadow": "0 10px 15px -3px rgba(0,0,0,0.05), 0 4px 6px -2px rgba(0,0,0,0.025)",
+            "fontFamily": "system-ui, -apple-system, sans-serif" 
+        }
     )
